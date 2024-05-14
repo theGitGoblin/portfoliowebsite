515ddec3cefcfd9d1200f2d997427ecb5619c63f
Jay <149704927+jayy0719@users.noreply.github.com>
2024-05-13 22:33:33 -0400

    Updated links to my socials.

--- app/page.tsx
+++ app/page.tsx
@@ -11,10 +11,10 @@ export default function Home() {
         </Head> 
         <Image alt="Stars" src={starpixel} quality={100} fill sizes="100vw" style={{ objectFit: 'cover' , }} />
     <div className="fixed top-5 right-5 flex space-x-4 z-50">
-      <a className="transition-transform transform hover:scale-125" href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">
+      <a className="transition-transform transform hover:scale-125" href="https://www.linkedin.com/in/jeron-clarke/" target="_blank" rel="noopener noreferrer">
         <img className="w-8 h-8 rounded-full" src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" alt="LinkedIn" />
       </a>
-      <a className="transition-transform transform hover:scale-125" href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer">
+      <a className="transition-transform transform hover:scale-125" href="https://github.com/Codeizfun" target="_blank" rel="noopener noreferrer">
         <img className="w-8 h-8 rounded-full" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" />
       </a>
       <a className="transition-transform transform hover:scale-125" href="mailto:youremail@example.com">
