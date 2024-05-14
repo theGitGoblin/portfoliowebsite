b5ebee142ae844e3e5a20a722312190142debeb9
Jay <149704927+jayy0719@users.noreply.github.com>
2024-05-13 22:28:32 -0400

    Iterated and chose a new background.

--- app/page.tsx
+++ app/page.tsx
@@ -2,50 +2,33 @@ import Image from "next/image";
 import Head from "next/head";
 import Link from "next/link";
 import convention from "./convention.png"
+import starpixel from "/app/Icons/starpixel.gif"
 export default function Home() {
   return (
-    <main className= "bg-white backgrounbd-size: auto;pt-px">
-      
+    <main className= "bg-black min-h-screen">
         <Head>
           <link href="globals.css" rel="stylesheet"></link>
-        </Head>
-        <div className="bg-sky-900 background-attachment: fixed; p-2 ">
-          <header className="text-white text-align: left;"> Portfolio </header>
-        </div>
-        <div className="clearfix">
-          <Image className="float-right mr-4 h-80 w-80 border-radius: 1.5rem; transition-transform duration-300 transform hover:scale-105 inline-block" src={convention}></Image>
-          </div>
-        <h1 className=" inline-flex p-6 text font-mono text-sky-900 font-extrabold text-xl"> Jeron Clarke </h1>
-        <div >
+        </Head> 
+        <Image alt="Stars" src={starpixel} quality={100} fill sizes="100vw" style={{ objectFit: 'cover' , }} />
+    <div className="fixed top-5 right-5 flex space-x-4 z-50">
+      <a className="transition-transform transform hover:scale-125" href="https://www.linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">
+        <img className="w-8 h-8 rounded-full" src="https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png" alt="LinkedIn" />
+      </a>
+      <a className="transition-transform transform hover:scale-125" href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer">
+        <img className="w-8 h-8 rounded-full" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" />
+      </a>
+      <a className="transition-transform transform hover:scale-125" href="mailto:youremail@example.com">
+        <img className="w-8 h-8 rounded-full" src="https://www.flaticon.com/free-icon/gmail_732200?term=mail&page=1&position=9&origin=tag&related_id=732200" alt="Email" />
+      </a>
+    </div>
+     <div className="relative z-10 flex flex-col items-center justify-center h-full">
+        <span className=" absolute inset-y-40 left-16  text-white font-extrabold font-sans ">
+          Hello! <br></br>My name is Jeron Clarke.
+        </span>
+      </div>
+  
         
-        </div>      
         
-        <h2 className=" inline-block mr-px p-20 text font-mono  text-sky-900 font-extrabold text-3xl">About Me </h2>
-         
-          <p className="text-right p-15 inline-table box-sizing:128 border-box 128; " >
-            Hello! My name is Jeron Clarke and I am a Computer Science student craving for learning!
-            I have recently pivoted from pre-med to a career in technology due to my love of technology that I have been fostering since I was a child.
-            As of right now my skills/softwares consists of : HTML, CSS, TypeScript, Python, SpringBoot, React, Tailwind, Django.
-                              
-          </p>
-        <p>
-
-        </p>
-        <h4 className="inline-flex"> Current/ Previous Projects </h4>
-        <div className = "text-right border-status-0.5rem ">
-        <h5>My Links</h5>
-            <ul>
-            <li>
-            <a className="Personal" href= "https://www.linkedin.com/in/jeron-clarke/"> My LinkedIn</a>
-            </li>
-            <li>
-            <a className="Personal" href="https://github.com/Codeizfun"> My Github </a>
-            </li>
-            <li>
-            <a className="Personal"href="mailto:jeronclarke09@gmail.com">Send email</a>
-            </li>        
-            </ul> 
-          </div>
                             
                   
     </main>
